ed65537545b128ed38de923ca47ec1a9fb0a93ca
Ines Chavez <ineschavez@iness.macbook.pro.lan>
2025-09-09 17:02:24 -0400

    front-end talks to back-end

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
 
 // src/app/page.js
-export default function Home() {
+// This is the main page component for the JobCore application
+export default async function Home() {
+
+  const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+  //defining the base URL for the API request, using an environment variable if available, otherwise defaulting to localhost
+  const res = await fetch (`${baseUrl}/api/hello`);
+  //sending a get request to the api/hello endpoint
+  //awaiting the response before continuing
+  const data = await res.json();
+  //parsing the response as json
+  console.log("this is the data", data)
+
+
+
   return (
     <main>
       <h1 style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}>
